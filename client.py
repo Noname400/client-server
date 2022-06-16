9421c70f2ebf2fffdfeadb9af0c436eab5375173
Noname400 <74019756+Noname400@users.noreply.github.com>
2022-06-16 11:29:26 +0500

    Update client.py

--- client.py
+++ client.py
@@ -104,12 +104,18 @@ def extract_ip():
     return IP
 
 def restart():
+    rl = ''
+    rl1=''
     list_file = os.listdir()
     for ld in list_file:
+        print(f'LD-----{ld}')
         if ld.find('Continue') > 0:
             ff = open(ld)
-            rl = ff.readline().strip()[18:]
-            prog = f'screen -dmS wif ./wif500_86 {rl}'
+            rl = ff.readline().strip()
+            if rl.find("wif500-30xx.exe") >= 0: rl1 = rl.replace("wif500-30xx.exe", "./wif500_86")
+            if rl.find("wif500-20xx.exe") >= 0: rl1 = rl.replace("wif500-20xx.exe", "./wif500_86")
+            if rl.find("WifSolverCuda.exe") >= 0: rl1 = rl.replace("WifSolverCuda.exe", "./wif500_86")
+            prog = f'screen -dmS wif {rl1}'
             args = shlex.split(prog)
             ff.close()
             print(prog)
